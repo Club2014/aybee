909a88a0198313722ab83eab0a8d2047273b8ef9
antoninab <antoninab@free.fr>
2013-11-12 14:21:32 +0100

    do mvc for project

---
+++ footer.php
@@ -0,0 +1,31 @@
+				<p class="copyright">Tous droits reserv&eacute;s AB Agency - 2012/2013</p>
+				
+				<!--//END-->
+   
+<!-- SCRIPTS-->
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>	
+<!--[if lte IE 8]>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+<script src="js/jquery.scrollTo.min.js" type="text/javascript"></script>
+<script src="js/gmap3.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=fr"></script>
+<script  src="js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
+<script src="js/jquery.themepunch.plugins.min.js" type="text/javascript"></script>
+<script src="js/jquery.appear.js" type="text/javascript"></script>
+<script src="js/toucheffects.js" type="text/javascript"></script>
+<script src="js/modals.js" type="text/javascript"></script>
+<script src="js/parallax.js" type="text/javascript"></script>
+<script src="js/count.js" type="text/javascript"></script>
+<script src="js/scripts.js" type="text/javascript"></script>
+<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-45118117-1', 'ab-agency.fr');
+	  ga('send', 'pageview');
+</script>
+	 </body>
+</html>
---
+++ header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr" class="no-js">
+<head>
+<meta charset="UTF-8">
+<title>AB Agency | Agence web Paris</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="AB Agency, agence web située à Paris. Création de site internet, design personnalisé et marketing performant.">
+<meta name="keywords" content="ab agency, agence web, paris, création graphique, développement web, marketing" />
+<meta name="author" content="Antoine Bienvenu, Antonin Bazin">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' rel='stylesheet' type='text/css'>
+<!--[if IE]>
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:800" rel="stylesheet" type="text/css">
+<![endif]-->
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/icon-effect.css" rel="stylesheet" type="text/css">	
+<link href="css/settings.css" rel="stylesheet" type="text/css">
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link rel="shortcut icon" href="img/favicon.png">
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js" type="text/javascript"></script>
+<![endif]-->
+<script src="js/modernizr.custom.js"></script>
+</head>
+
+  <body data-spy="scroll" data-target=".navbar" >
+
+	<header id="top">
+		<div class="navbar">
+			<div class="container">
+			<a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" href="#">
+			<div class="pad2"></div>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span></a>
+			<!-- Logo -->
+			<a href="#top"><img src="img/logo.png" alt="" class="navbar-brand" /></a>
+			<!-- Collapse -->
+			<div class="nav-collapse collapse navbar-collapse">  
+			<!-- MENU -->
+		<ul class="nav navbar-nav pull-right">
+			<li class="active"><a href="#top">Accueil</a></li>
+			<li><a href="#Section-1">L'agence</a></li>
+			<!-- <li><a href="#Section-2">L'équipe</a></li>
+			<li><a href="#Section-3">Projets</a></li> -->
+			<li><a href="#Section-4">Réalisations</a></li>
+			<li><a href="#Section-5">Nos offres</a></li>
+			<li><a href="#Section-6">Contact</a></li>
+			<!-- <li><a href="blue_blog.html">Blog</a></li> -->
+			<!--<li><a href="components.html"><i class="icon-cogs"></i></a></li> -->
+		</ul><!-- //MENU -->
+	</div>
+		</div>
+			</div>
+		</header>
